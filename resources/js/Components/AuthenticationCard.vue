ee3eb446e66b973a978e9045461c4e43936a672f
Samuca7CN-TCD <samuca.7cn@gmail.com>
2023-02-13 17:58:11 -0300

    Criação de models, migrations e controllers

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,3 +1,4 @@
+<!--
 <template>
     <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
         <div>
@@ -6,6 +7,25 @@
 
         <div class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white shadow-md overflow-hidden sm:rounded-lg">
             <slot />
+        </div>
+    </div>
+</template>
+-->
+
+<template>
+    <div class="w-screen h-screen flex flex-row justify-center justify-items-center">
+        <div
+            class="w-full h-screen px-10 md:p-0 lg:w-1/3 lg:h-screen bg-white flex flex-col justify-center items-center">
+            <div>
+                <slot name="logo" />
+            </div>
+            <div
+                class="w-full sm:max-w-md mt-6 px-6 py-4 bg-white transition-shadow shadow-md hover:shadow-2xl overflow-hidden sm:rounded-lg">
+                <slot />
+            </div>
+        </div>
+        <div
+            class="w-0 h-0 lg:w-2/3 lg:h-screen flex flex-col justify-center justify-items-center bg-center bg-no-repeat bg-cover bg-[url('/storage/app/public/theme/login/logo.svg')]">
         </div>
     </div>
 </template>
--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,9 @@ use Inertia\Inertia;
 |
 */
 
+require_once 'fortify.php';
+
+/*
 Route::get('/', function () {
     return Inertia::render('Welcome', [
         'canLogin' => Route::has('login'),
@@ -23,13 +26,14 @@ Route::get('/', function () {
         'phpVersion' => PHP_VERSION,
     ]);
 });
+*/
 
 Route::middleware([
     'auth:sanctum',
     config('jetstream.auth_session'),
     'verified',
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/', function () {
         return Inertia::render('Dashboard');
     })->name('dashboard');
 });

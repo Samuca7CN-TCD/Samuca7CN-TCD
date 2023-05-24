fa61ee60f6a2e6bd61563d7d4e97aed17a88a308
Samuca7CN-TCD <samuca.7cn@gmail.com>
2023-05-23 23:48:29 -0300

    Teste

---
+++ public/build/assets/shared_functions-016de35d.js
@@ -0,0 +1,1 @@
+const n=r=>{if(r!==null)return r.toLocaleString("pt-br",{style:"currency",currency:"BRL"})},t=r=>(r=r.split(/\-|\ |\:|\T|\./),r[2]+"/"+r[1]+"/"+r[0]+" às "+r[3]+":"+r[4]);export{t as f,n as t};
